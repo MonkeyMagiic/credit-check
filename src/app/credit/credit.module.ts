0d51c9c0528c05b7d381d73fb8ac0e932c7b1b58
Tyrone Neill <tyrone_neill@yahoo.co.uk>
2017-10-08 19:28:13 +0100

    fixing async dependency issues

--- src/app/credit/credit.module.ts
+++ src/app/credit/credit.module.ts
@@ -1,7 +1,10 @@
 import {NgModule} from "@angular/core";
+import {CommonModule} from '@angular/common';
 import {CreditOverview, CreditScore, CreditPosition} from "./credit-overview.component";
+import {IndicatorsModule} from "../indicators/indicators.module";
 
 @NgModule({
+    imports: [CommonModule, IndicatorsModule],
     declarations: [CreditOverview, CreditScore, CreditPosition],
     exports: [CreditOverview]
 })
